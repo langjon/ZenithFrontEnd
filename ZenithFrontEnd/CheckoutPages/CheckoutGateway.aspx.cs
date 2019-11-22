ebd8b1926ce2f4a8b15cbf690069099e2d7f3e38
langjon <langjon@sheridancollege.ca>
2019-11-22 13:02:40 -0500

    mvc fix

--- ZenithFrontEnd/CheckoutPages/CheckoutGateway.aspx.cs
+++ ZenithFrontEnd/CheckoutPages/CheckoutGateway.aspx.cs
@@ -31,10 +31,11 @@ namespace ZenithFrontEnd.CheckoutPages
                 Response.Write("<input type='hidden' name='item_number' value='0'>");
                 Response.Write("<input type='hidden' name='amount' value='" + Session["cartTotal"].ToString() + "'>");
                 Response.Write("<input type='hidden' name='no_shipping' value='1'>");
-                Response.Write("<input type='hidden' name='return' value='http://cbpackaging.azurewebsites.net/CheckoutPages/CheckoutSuccess.aspx?order=" + orderNo.ToString() + "'>");
+                //Response.Write("<input type='hidden' name='return' value='http://cbpackaging.azurewebsites.net/CheckoutPages/CheckoutSuccess.aspx?order=" + orderNo.ToString() + "'>");
                 //Response.Write("<input type='hidden' name='return' value='https://localhost:44308/CheckoutPages/CheckoutSuccess.aspx?order=" + orderNo.ToString() + "'>"); 
                 //Response.Write("<input type='hidden' name='return' value='https://localhost:44308/CheckoutPages/PaypalResponse.aspx'>");
                 //Response.Write("<input type='hidden' name='return' value='http://cbpackaging.azurewebsites.net/CheckoutPages/PaypalResponse.aspx?order=" + orderNo.ToString() + "'>");
+                Response.Write("<input type='hidden' name='return' value='http://cbpackaging.azurewebsites.net/Controllers/IPNController.cs>");
                 Response.Write("<script type ='text/javascript'>");
                 Response.Write("document.getElementById('checkout').submit();");
                 Response.Write("</script>");
