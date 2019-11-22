96d8eb506f571913ba3850b3939e0933378db4a5
langjon <langjon@sheridancollege.ca>
2019-11-22 11:22:24 -0500

    paypal ipn test

--- ZenithFrontEnd/CheckoutPages/CheckoutSuccess.aspx
+++ ZenithFrontEnd/CheckoutPages/CheckoutSuccess.aspx
@@ -3,6 +3,7 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
     <h1>THANK YOU FOR YOUR ORDER</h1>
+    <p></p>
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
 </asp:Content>
---
+++ ZenithFrontEnd/CheckoutPages/PaypalResponse.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using PayPal.PayPalAPIInterfaceService;
+using PayPal.PayPalAPIInterfaceService.Model;
+
+namespace ZenithFrontEnd.CheckoutPages
+{
+    public partial class PaypalResponse : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ProcessPayPalIPNNotification();
+        }
+
+        private void ProcessPayPalIPNNotification()
+        {
+            // receive PayPal ipn data
+
+            // extract ipn data into a string
+            byte[] param = Request.BinaryRead(Request.ContentLength);
+            string strRequest = Encoding.ASCII.GetString(param);
+
+            // append PayPal verification code to end of string
+            strRequest += "&cmd=_notify-validate";
+
+            // create an HttpRequest channel to perform handshake with PayPal
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(@"https://www.paypal.com/cgi-bin/webscr");
+            req.Method = "POST";
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.ContentLength = strRequest.Length;
+
+            // send data back to PayPal to request verification
+            StreamWriter streamOut = new StreamWriter(req.GetRequestStream(), Encoding.ASCII);
+            streamOut.Write(strRequest);
+            streamOut.Close();
+
+            // receive response from PayPal
+            StreamReader streamIn = new StreamReader(req.GetResponse().GetResponseStream());
+            string strResponse = streamIn.ReadToEnd();
+            streamIn.Close();
+
+            // if PayPal response is successful / verified
+            if (strResponse.Equals("VERIFIED"))
+            {
+                // paypal has verified the data, it is safe for us to perform processing now
+
+                // extract the form fields expected: buyer and seller email, payment status, amount
+                string payerEmail = Request.Form["payer_email"];
+                string paymentStatus = Request.Form["payment_status"];
+                string receiverEmail = Request.Form["receiver_email"];
+                string amount = Request.Form["mc_gross"];
+                string transactionID = Request.Form["txn_id"];
+
+                Session["transactionID"] = transactionID.ToString();
+
+                txnID.Text = transactionID.ToString();
+                payerEmailText.Text = payerEmail.ToString();
+                statusText.Text = paymentStatus.ToString();
+                receiverEmailText.Text = receiverEmail.ToString();
+                amountText.Text = amount.ToString();
+            }
+        }
+    }
+}
