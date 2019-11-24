7a8aaf7b7553070fa3b61017dd67038f25b74187
faizajahanzeb <47185146+faizajahanzeb@users.noreply.github.com>
2019-11-24 11:37:25 -0500

    comit again for site mater

--- ZenithFrontEnd/Site.Master.cs
+++ ZenithFrontEnd/Site.Master.cs
@@ -22,7 +22,7 @@ namespace ZenithFrontEnd
                 navCart.InnerHtml = "";
                 navBuild.InnerHtml = "";
                 navProfile.InnerHtml = "";
-               navOrder.InnerHtml = "<a href=\"../EmployeeDashboard/Dashboard.aspx\">ORDERS</a>";
+                navOrder.InnerHtml = "<a href=\"../EmployeeDashboard/Dashboard.aspx\">ORDERS</a>";
                 navCust.InnerHtml = "<a href=\"../EmployeeDashboard/CustomersDetails.aspx\">CUSTOMERS</a>";
                 navEmp.InnerHtml = "<a href=\"../EmployeeDashboard/EmployeesDetails.aspx\">EMPLOYEES</a>";
                 welcomeLabel.Text = Session["UserName"].ToString().ToUpper() + "'S DASHBAORD";
--- ZenithFrontEnd/Site.Master.designer.cs
+++ ZenithFrontEnd/Site.Master.designer.cs
@@ -40,6 +40,15 @@ namespace ZenithFrontEnd {
         protected global::System.Web.UI.WebControls.Label welcomeLabel;
         
         /// <summary>
+        /// navProfile control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl navProfile;
+        
+        /// <summary>
         /// navCart control.
         /// </summary>
         /// <remarks>
@@ -65,15 +74,6 @@ namespace ZenithFrontEnd {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl navBuild;
-        
-        /// <summary>
-        /// navProfile control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl navProfile;
         
         /// <summary>
         /// navOrder control.
