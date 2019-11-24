d7a0c1104dc740fb5dc26ea6e623f95e9309ae15
Czarina <czarina01punzalan@yahoo.com>
2019-11-24 11:06:50 -0500

    up

--- ZenithFrontEnd/Site.Master.designer.cs
+++ ZenithFrontEnd/Site.Master.designer.cs
@@ -67,6 +67,15 @@ namespace ZenithFrontEnd {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl navBuild;
         
         /// <summary>
+        /// navProfile control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl navProfile;
+        
+        /// <summary>
         /// errorMessages control.
         /// </summary>
         /// <remarks>
